2ee8a66b915d8cab8efad65157cf8f1b9bb6c4de
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-25 01:00:30 +0200

    feat: init flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "An empty nix devshell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ ];
+        };
+      }
+    );
+}
