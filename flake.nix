b4c979a5c2e84ea431fa6b6e02f780c06f724aa4
Valyn Tyler <git@valyntyler.com>
2025-12-24 00:29:46 +0200

    refactor: moved to using wrappers

--- flake.nix
+++ flake.nix
@@ -3,9 +3,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    wrappers.url = "github:lassulus/wrappers";
   };
 
-  outputs = inputs @ {flake-parts, ...}:
+  outputs = inputs @ {
+    flake-parts,
+    wrappers,
+    ...
+  }:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
       perSystem = {pkgs, ...}: rec {
@@ -19,27 +24,25 @@
         };
 
         packages.default = packages.wofizilla;
-        packages.wofizilla = pkgs.stdenv.mkDerivation rec {
-          name = "wofizilla";
-          src = ./src;
-          buildInputs = with pkgs; [
-            makeWrapper
-            wofi
-            nushell
-            nushellPlugins.formats
-          ];
-          installPhase = ''
-            mkdir -p $out/bin
-            cp ./wofizilla.nu $out/bin/${name}
-            chmod +x $out/bin/${name}
-            wrapProgram $out/bin/${name} \
-              --prefix PATH : ${pkgs.lib.makeBinPath [
-              pkgs.wofi
-              pkgs.nushell
-              pkgs.nushellPlugins.formats
-            ]}
-          '';
-        };
+        packages.wofizilla = let
+          package = pkgs.stdenv.mkDerivation rec {
+            name = "wofizilla";
+            src = ./src;
+            installPhase = ''
+              mkdir -p $out/bin
+              cp ./wofizilla.nu $out/bin/${name}
+              chmod +x $out/bin/${name}
+            '';
+          };
+        in
+          wrappers.lib.wrapPackage {
+            inherit pkgs package;
+            runtimeInputs = with pkgs; [
+              wofi
+              nushell
+              nushellPlugins.formats
+            ];
+          };
       };
     };
 }
